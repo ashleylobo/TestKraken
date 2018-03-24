39289ae7f142a2a242741e39b4d2ed1720fb2c00
Nehal Kalnad <yenwiikae@gmail.com>
2018-03-24 15:46:12 +0530

    Run Server option for checkin
    running index page

--- EventManager/accounts/models.py
+++ EventManager/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+# class User(AbstractUser):
+#   USER_TYPE_CHOICES = (
+#       (1, 'student'),
+#       (2, 'committee'),
+#       (3, 'guest'),
+#   )
+#
+#   user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+#   name = models.CharField(max_length=50)
